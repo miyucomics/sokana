6eaf48100d648c5e1fe5083f43a1a44ad3d72786
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-12-07 23:22:59 -0500

    Initial commit

---
+++ src/client/java/miyucomics/sokana/SokanaDataGenerator.kt
@@ -0,0 +1,10 @@
+package miyucomics.sokana
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object SokanaDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+
+	}
+}
