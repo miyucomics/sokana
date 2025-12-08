ea0697af4b4352682b0fca8c8f5225a09d161555
miyucomics <nbriannyc@gmail.com>
2025-12-07 23:22:59 -0500

    Initial commit

---
+++ src/client/java/miyucomics/sokana/SokanaClient.kt
@@ -0,0 +1,9 @@
+package miyucomics.sokana
+
+import net.fabricmc.api.ClientModInitializer
+
+object SokanaClient : ClientModInitializer {
+	override fun onInitializeClient() {
+
+	}
+}
---
+++ src/main/java/miyucomics/sokana/SokanaMain.kt
@@ -0,0 +1,12 @@
+package miyucomics.sokana
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object SokanaMain : ModInitializer {
+    private val logger = LoggerFactory.getLogger("sokana")
+
+	override fun onInitialize() {
+
+	}
+}
