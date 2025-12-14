3232d1a7db2d3acdcc7a7086c8ca1d57824a6e29
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-12-14 13:14:58 -0500

    Started the spell engine

---
+++ src/main/java/miyucomics/sokana/magic/casting/SpellEngine.kt
@@ -0,0 +1,7 @@
+package miyucomics.sokana.magic.casting
+
+// Temporary construct, does not ever get serialized
+// Steps through a spell image to tick it forwards
+class SpellEngine(image: SpellImage) {
+
+}
