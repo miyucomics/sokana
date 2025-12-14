6f00ddb75b01fe401381c26b112e8cf822f1ef81
miyucomics <nbriannyc@gmail.com>
2025-12-14 13:17:13 -0500

    Initialized registries

--- src/main/java/miyucomics/sokana/SokanaMain.kt
+++ src/main/java/miyucomics/sokana/SokanaMain.kt
@@ -1,12 +1,16 @@
 package miyucomics.sokana
 
+import miyucomics.sokana.magic.atoms.AtomTypes
+import miyucomics.sokana.magic.continuations.ContinuationFrameTypes
 import net.fabricmc.api.ModInitializer
-import org.slf4j.LoggerFactory
+import net.minecraft.util.Identifier
 
 object SokanaMain : ModInitializer {
-    private val logger = LoggerFactory.getLogger("sokana")
+    private const val MOD_ID = "sokana"
+	fun id(path: String): Identifier = Identifier.of(MOD_ID, path)
 
 	override fun onInitialize() {
-
+		AtomTypes.init()
+		ContinuationFrameTypes.init()
 	}
 }
