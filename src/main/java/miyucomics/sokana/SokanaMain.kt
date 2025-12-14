593d2c134211ceab4a9c71d62c01cdbaf0cf18b4
miyucomics <nbriannyc@gmail.com>
2025-12-14 13:20:48 -0500

    Created dummy item for testing purposes

--- src/main/java/miyucomics/sokana/SokanaMain.kt
+++ src/main/java/miyucomics/sokana/SokanaMain.kt
@@ -1,16 +1,20 @@
 package miyucomics.sokana
 
+import miyucomics.sokana.items.DummyItem
 import miyucomics.sokana.magic.atoms.AtomTypes
 import miyucomics.sokana.magic.continuations.ContinuationFrameTypes
 import net.fabricmc.api.ModInitializer
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
 
 object SokanaMain : ModInitializer {
-    private const val MOD_ID = "sokana"
-	fun id(path: String): Identifier = Identifier.of(MOD_ID, path)
-
 	override fun onInitialize() {
 		AtomTypes.init()
 		ContinuationFrameTypes.init()
+		Registry.register(Registries.ITEM, id("dummy"), DummyItem())
 	}
+
+    private const val MOD_ID = "sokana"
+	fun id(path: String): Identifier = Identifier.of(MOD_ID, path)
 }
