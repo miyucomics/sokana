cc064715c84c4a37140dc6b61133550c15b92638
miyucomics <72044529+miyucomics@users.noreply.github.com>
2025-12-15 14:08:06 -0500

    Entity atoms

--- src/main/java/miyucomics/sokana/engine/parsers/impls/AtomParser.kt
+++ src/main/java/miyucomics/sokana/engine/parsers/impls/AtomParser.kt
@@ -7,12 +7,15 @@ import miyucomics.sokana.engine.parsers.StackParser
 import net.minecraft.server.world.ServerWorld
 
 // Parses for a specific atom type, has a purifier function that lets it transform that atom into the data it contains for ease
-class AtomParser<T : Atom, A : Any>(val type: AtomType<T>, val purifier: (T) -> A) : StackParser<A> {
+class AtomParser<T : Atom, A : Any>(val type: AtomType<T>, val purifier: (T, ServerWorld) -> A?) : StackParser<A> {
+	constructor(type: AtomType<T>, purifier: (T) -> A?) : this(type, { atom, world -> purifier(atom) })
+
 	override fun parse(stack: List<Atom>, startIdx: Int, world: ServerWorld): ParseResult<A>? {
 		val atom = stack.getOrNull(startIdx) ?: return null
 		if (atom.type == type) {
 			@Suppress("UNCHECKED_CAST")
-			return ParseResult(purifier(atom as T), 1)
+			val returnValue = purifier(atom as T, world) ?: return null
+			return ParseResult(returnValue, 1)
 		}
 		return null
 	}
